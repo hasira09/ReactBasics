47803e2fd5e9fd2efb8267727c86da3c6b38d8aa
hasira09 <hasiramahel09@gmail.com>
2023-05-24 18:03:34 +0530

    Click Events
    Events and Clicks

--- firstapp/src/Home.js
+++ firstapp/src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello, Hasira!', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target);
+    }
+
     return (
         <div className="Home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain('Tharushi', e)}>Click Me Again</button>
         </div>
     );
 }
  
-export default Home;
+export default Home; 
