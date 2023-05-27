aaa1dfc36e9f047d1d74f1ee4a83eb4263c7d2b3
hasira09 <hasiramahel09@gmail.com>
2023-05-28 01:04:34 +0530

    Reusing Components
    React Dev Tools, Outputting Lists and Props as well

--- firstapp/src/Home.js
+++ firstapp/src/Home.js
@@ -1,22 +1,17 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My New Web Page', body: 'lorem ipsum...', author: 'Hasira', id: 1},
         { title: 'Welcome to React', body: 'lorem ipsum...', author: 'Mahel', id: 2},
         { title: 'React Dev Tools', body: 'lorem ipsum...', author: 'Tharushi', id: 3}
-    ])
-    return (
-        <div className="Home">
-            <div className="home">
-                {blogs.map((blog) => (
-                    <div className="blog-preview" key={blog.id}>
-                        <h2>Written by { blog.title }</h2>
-                        <p>Written by { blog.author}</p>
+    ]);
 
-                    </div>
-                ))}
-            </div>
+    return (
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Hasira')} title="Hasira's Blogs" />
         </div>
     );
 }
